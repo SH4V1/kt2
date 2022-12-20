757801f639aff61d27aa328a3cdcf90d073ab759
SH4V1 <100089539+SH4V1@users.noreply.github.com>
2022-12-20 18:25:26 +0200

    Add files via upload

---
+++ kt2-main/src/components/Mid/index.js
@@ -0,0 +1,157 @@
+import React from "react";
+
+
+function Mid() {
+    return (
+        <div>
+        <div class="site-section section-2 bg-light" id="process-section" data-aos="fade">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 mb-5">
+                        <h2 class="section-title">About me</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem possimus distinctio ex. Natus totam voluptatibus animi aspernatur ducimus quas obcaecati mollitia quibusdam temporibus culpa dolore molestias blanditiis consequuntur sunt nisi.</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+                        <div class="process bg-white rounded">
+                            <div>
+                                <span class="flaticon-glasses display-4 text-primary mb-4 d-inline-block"></span>
+                                <h3>Deep Learning</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem possimus distinctio ex. Natus totam voluptatibus animi aspernatur ducimus quas obcaecati mollitia quibusdam temporibus culpa dolore molestias blanditiis consequuntur sunt nisi.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+                        <div class="process bg-white rounded">
+                            <div>
+                                <span class="flaticon-vector display-4 text-primary mb-4 d-inline-block"></span>
+                                <h3>Working on Projects</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem possimus distinctio ex. Natus totam voluptatibus animi aspernatur ducimus quas obcaecati mollitia quibusdam temporibus culpa dolore molestias blanditiis consequuntur sunt nisi.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+                        <div class="process bg-white rounded">
+                            <div>
+                                <span class="flaticon-monitor display-4 text-primary mb-4 d-inline-block"></span>
+                                <h3>Release</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem possimus distinctio ex. Natus totam voluptatibus animi aspernatur ducimus quas obcaecati mollitia quibusdam temporibus culpa dolore molestias blanditiis consequuntur sunt nisi.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="site-section testimonial-wrap custom-owl-carousel" id="testimonials-section">
+            <div class="container">
+                <div class="row text-center justify-content-center mb-5" data-aos="fade-up">
+                    <div class="col-lg-6 mb-5 text-center">
+                        <h2 class="section-title w-100 mx-auto">My Photos</h2>
+                        <p>Natus totam voluptatibus animi aspernatur ducimus quas obcaecati mollitia quibusdam temporibus culpa dolore molestias blanditiis consequuntur sunt nisi.</p>
+                    </div>
+                </div>
+                <div class="row align-items-center" data-aos="fade-up" data-aos-delay="100">
+                    <div class="col-lg-5 mr-auto">
+                        <div class="owl-carousel slide-one-item-alt-text">
+                            <div class="slide-text">
+                                <blockquote>
+                                    <p><span>&ldquo;</span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos corrupti, obcaecati repudiandae, animi ad quos. Eius expedita ipsa qui autem.<span>&rdquo;</span></p>
+                                    <p class="author">&mdash; Roman Davidyuk</p>
+                                </blockquote>
+                            </div>
+                            <div class="slide-text">
+                                <blockquote>
+                                    <p><span>&ldquo;</span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos corrupti, obcaecati repudiandae, animi ad quos. Eius expedita ipsa qui autem.<span>&rdquo;</span></p>
+                                    <p class="author">&mdash; Roman Davidyuk</p>
+                                </blockquote>
+                            </div>
+                            <div class="slide-text">
+                                <blockquote>
+                                    <p><span>&ldquo;</span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos corrupti, obcaecati repudiandae, animi ad quos. Eius expedita ipsa qui autem.<span>&rdquo;</span></p>
+                                    <p class="author">&mdash; Roman Davidyuk</p>
+                                </blockquote>
+                            </div>
+                            <div class="slide-text">
+                                <blockquote>
+                                    <p><span>&ldquo;</span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos corrupti, obcaecati repudiandae, animi ad quos. Eius expedita ipsa qui autem.<span>&rdquo;</span></p>
+                                    <p class="author">&mdash; Roman Davidyuk</p>
+                                </blockquote>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 ml-auto" data-aos="fade-up" data-aos-delay="200">
+                        <div class="owl-carousel slide-one-item-alt">
+                            <img src="./images/p1.jpg" alt="Image" class="img-fluid"></img>
+                            <img src="./images/p2.jpg" alt="Image" class="img-fluid"></img>
+                            <img src="./images/p3.jpg" alt="Image" class="img-fluid"></img>
+                            <img src="./images/p4.jpg" alt="Image" class="img-fluid"></img>
+                        </div>
+                        <div class="owl-custom-direction">
+                            <a href="#" class="custom-prev"><span class="icon-keyboard_arrow_left"></span></a>
+                            <a href="#" class="custom-next"><span class="icon-keyboard_arrow_right"></span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="site-section bg-light" id="services-section" data-aos="fade">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 mb-5">
+                        <h2 class="section-title">Skills</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem possimus distinctio ex. Natus totam voluptatibus animi aspernatur ducimus quas obcaecati mollitia quibusdam temporibus culpa dolore molestias blanditiis consequuntur sunt nisi.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="owl-carousel nonloop-block-14">
+                <div class="service">
+                    <div>
+                        <span class="custom-icon-wrap"><span class="flaticon-reload"></span></span>
+                        <h3>Python Developer</h3>
+                        <p>Natus totam voluptatibus animi aspernatur ducimus quas obcaecati mollitia quibusdam temporibus culpa dolore molestias blanditiis consequuntur sunt nisi.</p>
+                    </div>
+                </div>
+                <div class="service">
+                    <div>
+                        <span class="custom-icon-wrap"><span class="flaticon-download"></span></span>
+                        <h3>Downloadble Projects</h3>
+                        <p>Natus totam voluptatibus animi aspernatur ducimus quas obcaecati mollitia quibusdam temporibus culpa dolore molestias blanditiis consequuntur sunt nisi.</p>
+                    </div>
+                </div>
+                <div class="service">
+                    <div>
+                        <span class="custom-icon-wrap"><span class="flaticon-monitor"></span></span>
+                        <h3>Web Developer</h3>
+                        <p>Natus totam voluptatibus animi aspernatur ducimus quas obcaecati mollitia quibusdam temporibus culpa dolore molestias blanditiis consequuntur sunt nisi.</p>
+                    </div>
+                </div>
+                <div class="service">
+                    <div>
+                        <span class="custom-icon-wrap"><span class="flaticon-chat"></span></span>
+                        <h3>HTML5/CSS3</h3>
+                        <p>Natus totam voluptatibus animi aspernatur ducimus quas obcaecati mollitia quibusdam temporibus culpa dolore molestias blanditiis consequuntur sunt nisi.</p>
+                    </div>
+                </div>
+                <div class="service">
+                    <div>
+                        <span class="custom-icon-wrap"><span class="flaticon-glasses"></span></span>
+                        <h3>C++</h3>
+                        <p>Natus totam voluptatibus animi aspernatur ducimus quas obcaecati mollitia quibusdam temporibus culpa dolore molestias blanditiis consequuntur sunt nisi.</p>
+                    </div>
+                </div>
+                <div class="service">
+                    <div>
+                        <span class="custom-icon-wrap"><span class="flaticon-vector"></span></span>
+                        <h3>Designer</h3>
+                        <p>Natus totam voluptatibus animi aspernatur ducimus quas obcaecati mollitia quibusdam temporibus culpa dolore molestias blanditiis consequuntur sunt nisi.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+      
+    );
+  }
+
+  export default Mid;
